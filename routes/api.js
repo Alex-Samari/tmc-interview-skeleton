5bea28bf9b81888154de936da8cf5ddc5ffe67ae
Alex Samari <alisamarim7@gmail.com>
2021-07-23 16:01:29 -0400

    implement api/cities/:province api route

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-var db = require('../data/db')
+var db = require('../data/db');
 
 // API routes
-router.get('/provinces', function(req, res, next) {
-  res.json(db.provinces)
+router.get('/provinces', function (req, res, next) {
+  res.json(db.provinces);
 });
 
-router.get('/cities', function(req, res, next) {
-  res.json(db.cities)
+router.get('/cities', function (req, res, next) {
+  res.json(db.cities);
+});
+
+router.get('/cities/:province', function (req, res, next) {
+  const cities = db.cities.filter(
+    (city) => city.Province === req.params.province
+  );
+  res.json(cities);
 });
 
 module.exports = router;
